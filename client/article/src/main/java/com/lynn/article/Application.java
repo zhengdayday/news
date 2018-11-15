0e40de076e47d22134ec7dd713e3836c389ec1ee
zhengdayday <1157790064@qq.com>
2018-11-15 15:06:09 +0800

    Initial commit

---
+++ client/article/src/main/java/com/lynn/article/Application.java
@@ -0,0 +1,22 @@
+package com.lynn.article;
+
+import com.lynn.common.annotation.EnableEncrypt;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+@EnableHystrixDashboard
+@EnableCircuitBreaker
+@EnableEncrypt
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
